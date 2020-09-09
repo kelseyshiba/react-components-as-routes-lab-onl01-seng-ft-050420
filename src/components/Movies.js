d10d50f00e954aa6cbf9b43cd0d783465799d7b9
Kelsey Shiba <kelsey.shiba@gmail.com>
2020-09-09 11:36:14 -0700

    Done.

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import { movies } from '../data';
 
-const Movies = () => {
+const Movies = (props) => {
   return (
     <div>
-        {/*{code here}*/}
+       <h1>Movies Page</h1>
+         {movies.map(movie=>
+         <div> 
+            <h3>{movie.title}</h3>
+            <p>Runtime: {movie.time}</p> 
+         <ul>{movie.genres.map(genre=> <li>{genre}</li>)}</ul>
+        </div>
+         )}
     </div>
   );
 };
